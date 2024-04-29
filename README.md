2f3da8b408467bdf58df6029b67b714be0d49e92
Faris Music <ffmusic.98@gmail.com>
2024-04-29 18:05:23 +0200

    created run.sh script to handle application behaviour

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# docker-spring-node
+# basic-microservices-docker
+
+Steps to run applications:
+
+1. Execute command to make a run.sh script executable:
+```
+chmod +x run.sh
+```
+
+2. Start applications using following command:
+```
+./run.sh start
+```
+
+3. Test applications using following command:
+```
+./run.sh test
+```
+*Note: Before testing application make sure that you proceeded with step 2.*
+
+4. Stop applications using following command:
+```
+./run.sh stop [--delete-volumes]
+```
+
---
+++ run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function run_test {
+    echo "Running tests..."
+    bash <(curl -s https://raw.githubusercontent.com/kkenan/basic-microservices/master/health_check.sh)
+}
+
+function start_app {
+    echo "Starting the application..."
+    docker compose up -d
+}
+
+function stop_app {
+    delete_volumes="$2"
+    if [ -n "$delete_volumes" ]; then
+      if [ "$delete_volumes" == "--delete-volumes" ]; then
+        echo "Stopping the application..."
+        docker compose down -v
+      else
+        echo "Unknown flag $delete_volumes"
+      fi
+    elif [ -z "$delete_volumes" ]; then
+      echo "Stopping the application..."
+        docker compose down
+    fi
+}
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 {test|start|stop [--delete-volumes]}"
+    exit 1
+fi
+
+case "$1" in
+    test)
+        run_test
+        ;;
+    start)
+        start_app
+        ;;
+    stop)
+        stop_app "$@"
+        ;;
+    *)
+        echo "Invalid argument: $1"
+        echo "Usage: $0 {test|start}"
+        exit 1
+        ;;
+esac
+
+exit 0
